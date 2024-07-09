cf374c472cf8eeb1ffc4b1a6d77e6073a01b0052
Arumyska <102806514+skazalbog@users.noreply.github.com>
2024-07-09 15:09:42 -0500

    PULL3

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LoginResponse } from '../types/login-response.type';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  Saidurl= "http://localhost:8080/auth/login";
+  constructor(private httpClient: HttpClient) { }
+
+  login(email: string, password: string){
+    return this.httpClient.post<LoginResponse>(this.Saidurl,{ email, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth-token", value.token)
+        sessionStorage.setItem("username", value.name)
+      })
+    )
+  }
+}
